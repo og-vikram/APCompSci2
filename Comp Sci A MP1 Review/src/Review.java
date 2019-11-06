ba644de20e43949108c20d52802c6b3ba9897ca0
Vikram Sridhar <mmarkiv7@gmail.com>
2019-11-06 13:22:58 -0500

    i took a fat shit

--- Comp Sci A MP1 Review/src/Review.java
+++ Comp Sci A MP1 Review/src/Review.java
@@ -3,25 +3,18 @@ import java.util.Scanner;
 public class Review {
 
 	public static void main(String[] args) {
-
+			
 		Scanner bot = new Scanner(System.in);
-		System.out.println("enter word");
+		System.out.println("Enter a word");
 		String word = bot.nextLine();
 		
-		if(word.length()>=1 && word.length()<=3){
-			System.out.println(word.substring(word.length()-1));
-		}
-	
-		else if(word.length()>3 && word.length()<7){
-			for(int i=0; i<word.length(); i+=2){
-				System.out.print(word.charAt(i));
-			}
-		}
-		else if(word.length()>=7){
-			System.out.println(word.substring(7,word.length()));
+		for(int i =0; i<word.length(); i+=2){
+		System.out.print(word.substring(i, i+1).toUpperCase());
+		System.out.print(word.substring(i).toLowerCase());
 		
+		
+		System.out.println(word);
+	
 		}
 	}
-	
-
 }
